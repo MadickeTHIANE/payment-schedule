ad483ba827e1be7dbe590826f33ce4dced91c8d3
Madické Thiane <madicke.thiane@canton-consulting.com>
2023-03-06 08:47:13 +0100

    Add table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
+import React from "react";
+import "./index.css";
+
 function App() {
-  return <div></div>;
+  const monthTable = [
+    "Janvier",
+    "Février",
+    "Mars",
+    "Avril",
+    "Mai",
+    "Juin",
+    "Juillet",
+    "Août",
+    "Septembre",
+    "Octobre",
+    "Novembre",
+    "Décembre",
+  ];
+
+  const totalAmountDue = 6000;
+  const monthlyPayment = 100;
+  const repaymentPeriod = totalAmountDue / monthlyPayment;
+  const repaymentRows = () => {
+    return (
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    );
+  };
+
+  React.useEffect(() => {
+    console.log("***repaymentPeriod : " + repaymentPeriod);
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Année</th>
+            <th>Mensualités</th>
+            <th>Cumule</th>
+            <th>Reste</th>
+            {/* {monthTable.map((month, id) => (
+            <th  key={id}>
+            {month}
+            </th>
+          ))} */}
+          </tr>
+        </thead>
+        <tbody>{repaymentRows()}</tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
